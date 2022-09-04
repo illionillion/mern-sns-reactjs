e5c7a973ed861cf03200263cc8faea0cfa2fcba4
yuto ichinose <yuto.0526.sano@gmail.com>
2022-09-04 17:49:37 +0900

    ライトバーコンポーネント完成、次回スタイル当て

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,6 +3,26 @@ import './Rightbar.css'
 
 export default function Rightbar() {
   return (
-    <aside className='rightbar'>Rightbar</aside>
+    <aside className='rightbar'>
+      <div className="rightbarWrapper">
+        <div className="eventContainer">
+          <img src="./assets/star.png" alt="イベント星マーク" className='startImg' />
+          <span className="eventText">
+            <b>フォロワー限定イベント開催！！</b>
+          </span>
+        </div>
+        <img src="./assets/event.jpeg" alt="イベント画像" className='' />
+        <h4 className="rightbarTitle">オンラインの友達</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="./assets/person/1.jpeg" alt="ユーザーアイコン" />
+              <span className="rightbarOnline">{/* ピンクのやつなので空 */}</span>
+            </div>
+            <span className="rightbarUsername">illion</span>
+          </li>
+        </ul>
+      </div>
+    </aside>
   )
 }
