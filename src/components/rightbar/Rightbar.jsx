d9ae23b0ad9a7831f9659f0d9ae324a40714fa99
yuto ichinose <yuto.0526.sano@gmail.com>
2022-09-04 18:03:26 +0900

    ライトバーのスタイル完成

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -6,17 +6,31 @@ export default function Rightbar() {
     <aside className='rightbar'>
       <div className="rightbarWrapper">
         <div className="eventContainer">
-          <img src="./assets/star.png" alt="イベント星マーク" className='startImg' />
+          <img src="./assets/star.png" alt="イベント星マーク" className='starImg' />
           <span className="eventText">
             <b>フォロワー限定イベント開催！！</b>
           </span>
         </div>
-        <img src="./assets/event.jpeg" alt="イベント画像" className='' />
+        <img src="./assets/event.jpeg" alt="イベント画像" className='eventImg' />
         <h4 className="rightbarTitle">オンラインの友達</h4>
         <ul className="rightbarFriendList">
           <li className="rightbarFriend">
             <div className="rightbarProfileImgContainer">
-              <img src="./assets/person/1.jpeg" alt="ユーザーアイコン" />
+              <img src="./assets/person/1.jpeg" alt="ユーザーアイコン" className='rightbarProfileImg' />
+              <span className="rightbarOnline">{/* ピンクのやつなので空 */}</span>
+            </div>
+            <span className="rightbarUsername">illion</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="./assets/person/1.jpeg" alt="ユーザーアイコン" className='rightbarProfileImg' />
+              <span className="rightbarOnline">{/* ピンクのやつなので空 */}</span>
+            </div>
+            <span className="rightbarUsername">illion</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="./assets/person/2.jpeg" alt="ユーザーアイコン" className='rightbarProfileImg' />
               <span className="rightbarOnline">{/* ピンクのやつなので空 */}</span>
             </div>
             <span className="rightbarUsername">illion</span>
