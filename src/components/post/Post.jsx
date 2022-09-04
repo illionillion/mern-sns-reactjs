759d663cde046b73ce125878436162920f029bb0
yuto ichinose <yuto.0526.sano@gmail.com>
2022-09-04 20:01:16 +0900

    タイムラインをダミーデータを元にデモ風

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,35 +1,41 @@
 import { MoreVert } from '@mui/icons-material'
 import React from 'react'
 import './Post.css'
+import { Users } from '../../dummyData';
 
-export default function Post() {
-  return (
-    <article className='post'>
-        <div className="postWrapper">
-            <div className="postTop">
-                <div className="postTopLeft">
-                    <img src='./assets/person/1.jpeg' alt="投稿アイコン" className='postProfileImg' />
-                    <span className="postUsername">illion</span>
-                    <span className="postDate">5分前</span>
+export default function Post({post}) {
+    // 引数のPOSTを取得
+
+    const user = Users.filter(user => user.id === post.id)[0] // filterでIDが同じものを取ってくる
+    // console.log(user);
+    
+    return (
+        <article className='post'>
+            <div className="postWrapper">
+                <div className="postTop">
+                    <div className="postTopLeft">
+                        <img src={user.profilePicture} alt="投稿アイコン" className='postProfileImg' />
+                        <span className="postUsername">{user.username}</span>
+                        <span className="postDate">{post.date}</span>
+                    </div>
+                    <div className="postTopRight">
+                        <MoreVert />
+                    </div>
                 </div>
-                <div className="postTopRight">
-                    <MoreVert />
+                <div className="postCenter">
+                    <span className="postText">{post.desc}</span>
+                    <img src={post.photo} alt="投稿画像" className='postImg' />
+                </div>
+                <div className="postBottom">
+                    <div className="postBottomLeft">
+                        <img src="./assets/heart.png" alt="いいねアイコン" className='likeIcon' />
+                        <span className="postLikeCounter">{post.like}人がいいねをしました</span>
+                    </div>
+                    <div className="postBottomRight">
+                        <span className="postCommentText">{post.comment}:コメント</span>
+                    </div>
                 </div>
             </div>
-            <div className="postCenter">
-                <span className="postText">SNS作ってる</span>
-                <img src="./assets/post/1.jpeg" alt="投稿画像" className='postImg' />
-            </div>
-            <div className="postBottom">
-                <div className="postBottomLeft">
-                    <img src="./assets/heart.png" alt="いいねアイコン" className='likeIcon' />
-                    <span className="postLikeCounter">5人がいいねをしました</span>
-                </div>
-                <div className="postBottomRight">
-                    <span className="postCommentText">4:コメント</span>
-                </div>
-            </div>
-        </div>
-    </article>
-  )
+        </article>
+    )
 }
--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Post from '../post/Post'
 import Share from '../share/Share'
 import './TimeLine.css'
+import { Posts } from '../../dummyData'
 
 export default function Timeline() {
   return (
     <section className='timeline'>
       <div className="timelineWrapper">
         <Share />
-        <Post />
+        {Posts.map(post => (
+          <Post post = {post} key = {post.id}/>
+        ))}
+        {/* <Post /> */}
       </div>
     </section>
   )
