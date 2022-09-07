af453992060cefeec4b24f373f4f0e6cfe3745d5
yuto ichinose <yuto.0526.sano@gmail.com>
2022-09-07 23:53:52 +0900

    ログイン画面

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -9,7 +9,16 @@ export default function Login() {
                 <h3 className='loginLogo'>Real-SNS</h3>
                 <span className="loginDesc">自作のSNS</span>
             </div>
-            <div className="loginRight">フォーム</div>
+            <div className="loginRight">
+              <div className="loginBox">
+                <p className="loginMsg">ログインはこちら</p>
+                <input type="text" className="loginInput" placeholder='Eメール' />
+                <input type="text" className="loginInput" placeholder='パスワード' />
+                <input type="button" value="ログイン" className="loginButton" />
+                <span className="loginForgot">パスワードを忘れた場合</span>
+                <input type="button" value="アカウント作成" className="loginRegisterButton" />
+              </div>
+            </div>
         </div>
     </div>
   )
