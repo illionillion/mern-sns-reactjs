90f503bd6398cfa1074592d5046c190c1eb59343
yuto ichinose <yuto.0526.sano@gmail.com>
2022-09-17 08:37:40 +0900

    パラメーターからクエリーに変更

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import Rightbar from '../../components/rightbar/Rightbar'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Timeline from '../../components/timeline/TimeLine'
@@ -7,6 +8,23 @@ import imageRequire from '../../imageRequire'
 import './Profile.css'
 
 export default function Profile() {
+
+  const [user, setUser] = useState([])
+
+    // useEffectでDOMにマウント時に一回だけ実行
+    useEffect(() => {
+
+    const fetchUser = async () => {
+        // プロキシ設定してるのでhttp~は省略
+        const response = await axios.get(`/users?username=illlion2`) // なぜか3000番にリクエスト送られて取れない
+        console.log(response);
+        setUser(response.data) // 中身を取り出す
+    }
+
+    fetchUser()
+
+    }, [])
+
   return (
     <>
       <Topbar />
@@ -19,8 +37,8 @@ export default function Profile() {
                 <img src={imageRequire("assets/person/1.jpeg")} alt="アイコン" className='profileUserImg' />
               </div>
               <div className="profileInfo">
-                <h4 className='profileInfoName'>illion</h4>
-                <span className="profileInfoDesc">学生プログラマー</span>
+                <h4 className='profileInfoName'>{user.username}</h4>
+                <span className="profileInfoDesc">{user.desc}</span>
               </div>
             </div>
             <div className="profileRightBottom">
