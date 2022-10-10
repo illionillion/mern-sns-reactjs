8f40c0194b3805a766731e2dd70ec72732ec55b1
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-10 11:49:05 +0900

    Reducer

---
+++ src/state/AuthReducer.js
@@ -0,0 +1,32 @@
+// リデューサー：裏側の仕組み
+
+/**
+ * 
+ * @param {*} state 
+ * @param {*} action AuthAction
+ */
+const AuthReducer = (state, action) => {
+    switch (action.type) {
+        case "LOGIN_START":
+            return {
+                user: null,
+                isFetching: true,
+                error: false,
+            }
+        case "LOGIN_SUCCESS":
+            return {
+                user: action.payload,
+                isFetching: false,
+                error: false,
+            }
+        case "LOGIN_ERROR":
+            return {
+                user: null,
+                isFetching: false,
+                error: action.payload,
+            }
+    
+        default:
+            break;
+    }
+}
