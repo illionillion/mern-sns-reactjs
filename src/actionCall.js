4447a7da57845f28d8711d9f96356a212aa163a2
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-12 11:06:34 +0900

    綴りミスあったので修正

--- src/actionCall.js
+++ src/actionCall.js
@@ -1,12 +1,12 @@
 import axios from "axios"
 
 export const loginCall = async (user, dispatch) => {
-    
+
     dispatch({type: "LOGIN_START"}) // 通知
 
     try {
         // LOGIN_STARTが正常に行われた場合
-        const response = await axios.post("auth/login", user)
+        const response = await axios.post("/auth/login", user)
         dispatch({type: "LOGIN_SUCCESS", payload: response.data})
     } catch (error) {
         dispatch({type: "LOGIN_ERROR", payload: error})
