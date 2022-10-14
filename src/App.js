997a98ab1b6377a4c041bee10b599c78fa5103a2
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-14 17:54:03 +0900

    ログイン時のリダイレクト処理を書いた

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
-import { BrowserRouter as Router, Route, Routes, } from "react-router-dom";
+import { BrowserRouter as Router, Navigate, Route, Routes, } from "react-router-dom";
 import './style.css'
+import { useContext } from "react";
+import { AuthContext } from "./state/AuthContext";
 
 function App() {
+  
+  const { user } = useContext(AuthContext)
+
   return (
     // ルーティング
       <Router>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/register" element={<Register/>}/>
+          {/* userがないなら登録画面へ */}
+          <Route path="/" element={user ? <Home/> : <Navigate to="/register"/>} />
+          {/* userがある場合Homeコンポーネントへリダイレクト */}
+          <Route path="/login" element={user ? <Navigate to = "/" /> : <Login/>}/>
+          <Route path="/register" element={user ? <Navigate to = "/" /> : <Register/>}/>
           <Route path="/profile/:username" element={<Profile/>}/>
         </Routes>
       </Router>
