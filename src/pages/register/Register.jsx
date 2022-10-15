0a32a2bf9436f55f22984ac79b10a8f9582c217b
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-15 15:33:28 +0900

    各ボックスにref、フォーム送信時の関数

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Register.css'
 
 export default function Register() {
+
+  const username = useRef()
+  const email = useRef()
+  const password = useRef()
+  const passwordConfirmation = useRef()
+
+
+  /**
+   * フォーム送信
+   * 
+   * ログインボタンを押した時
+   * 
+   * @param {Event} e イベント
+   */
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    console.log(username);
+    console.log(email);
+  }
+
   return (
     <div className='login'>
         <div className="loginWrapper">
@@ -10,15 +31,15 @@ export default function Register() {
                 <span className="loginDesc">自作のSNS</span>
             </div>
             <div className="loginRight">
-              <div className="loginBox">
+              <form className="loginBox" onSubmit={handleSubmit}>
                 <p className="loginMsg">新規登録はこちら</p>
-                <input type="text" className="loginInput" placeholder='ユーザー名' />
-                <input type="text" className="loginInput" placeholder='Eメール' />
-                <input type="text" className="loginInput" placeholder='パスワード' />
-                <input type="text" className="loginInput" placeholder='確認用パスワード' />
-                <input type="button" value="サインアップ" className="loginButton" />
+                <input type="text" className="loginInput" placeholder='ユーザー名' ref={username} autoComplete='username' required/>
+                <input type="email" className="loginInput" placeholder='Eメール' ref={email} autoComplete='email' required/>
+                <input type="password" className="loginInput" placeholder='パスワード' minLength={6} ref={password} autoComplete='new-passsword' required/>
+                <input type="password" className="loginInput" placeholder='確認用パスワード' minLength={6} ref={passwordConfirmation} autoComplete='current-passsword' required/>
+                <input type="submit" value="サインアップ" className="loginButton" />
                 <input type="button" value="ログイン" className="loginRegisterButton" />
-              </div>
+              </form>
             </div>
         </div>
     </div>
