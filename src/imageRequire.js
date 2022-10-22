cb3704f201d2a45503cea44acedc80be8771d9cf
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-23 08:42:37 +0900

    画像をサーバーに移動に修正

--- src/imageRequire.js
+++ src/imageRequire.js
@@ -4,6 +4,10 @@
  * @returns {string} 絶対パス
  */
 export default function imageRequire (path){
-    const REACT_APP_PUBLIC_FOLDER = window.location.origin // url取得
-    return REACT_APP_PUBLIC_FOLDER + '/' + path
+    
+    // const REACT_APP_PUBLIC_FOLDER = window.location.origin // url取得
+    const REACT_APP_PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER
+    const uri = REACT_APP_PUBLIC_FOLDER + '/' + path.replace('assets/','') // assets/いらない
+    // console.log(uri);
+    return uri
 }
